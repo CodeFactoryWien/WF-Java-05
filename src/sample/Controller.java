62e101535b84b78a25014e7d984381ec11032c79
Wojciech Schlesinger <wojciech.schlesinger@gmail.com>
2020-01-17 17:29:22 +0100

    Client-App upload

---
+++ src/sample/Controller.java
@@ -0,0 +1,363 @@
+package sample;
+
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.util.Callback;
+
+import java.sql.*;
+
+import static java.sql.DriverManager.getConnection;
+
+public class Controller {
+
+    Connection con;
+    int pos;
+    float sum = 0;
+    float buffsum = 0;
+
+    @FXML private TableView coffeeproducts;
+    @FXML private TableView teaproducts;
+    @FXML private TableView softdrinksproducts;
+    @FXML private TableView shoppingcart;
+    @FXML private TableView energydrinksproducts;
+    @FXML private TableView orderListTable;
+
+    @FXML private TextField clientID;
+    @FXML private TextField productID;
+    @FXML private TextField productnameText;
+    @FXML private TextField singlepriceText;
+    @FXML private TextField bulkpriceText;
+    @FXML private TextField amountText;
+
+    @FXML private Label totalLabel;
+    @FXML private Label productDescLabel;
+
+    ObservableList teaproductsList = FXCollections.observableArrayList();
+    ObservableList coffeeproductsList = FXCollections.observableArrayList();
+    ObservableList softdrinksproductsList = FXCollections.observableArrayList();
+    ObservableList energydrinksproductsList = FXCollections.observableArrayList();
+
+    ObservableList orderList = FXCollections.observableArrayList();
+    ObservableList shoppingcartList = FXCollections.observableArrayList();
+    TableColumn<String, String> shoppingcartCol = new TableColumn<String, String>("Shoppingcart");
+
+    public void initialize() throws SQLException {
+        shoppingcartCol.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
+        shoppingcart.getColumns().add(shoppingcartCol);
+        //Connection to the DB, happens only once
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/feelgoodltd?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Europe/Berlin", "root","");
+        createCoffeeTable();
+        createTeaTable();
+        createSoftDrinksTable();
+        createEnergyDrinksTable();
+    }
+
+    public void selectItemcoffee() throws SQLException {
+        String selectedItem = coffeeproducts.getSelectionModel().getSelectedItem().toString();
+        String buff[] = selectedItem.split(", ");
+        System.out.println(buff[0]);
+        String buff3 = buff[0].substring(1);
+        System.out.println(buff3);
+        String buff2[] = buff[3].split("]");
+        productnameText.setText(buff[1]);
+        singlepriceText.setText(buff[2]);
+        bulkpriceText.setText(buff2[0]);
+        productID.setText(buff3);
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT description FROM product WHERE productID = "+buff3);
+        while (rs.next()){
+            productDescLabel.setText(rs.getString("description"));
+        }
+    }
+    public void selectItemtea() throws SQLException {
+        String selectedItem = teaproducts.getSelectionModel().getSelectedItem().toString();
+        String buff[] = selectedItem.split(", ");
+        System.out.println(buff[0]);
+        String buff3 = buff[0].substring(1);
+        System.out.println(buff3);
+        String buff2[] = buff[3].split("]");
+        productnameText.setText(buff[1]);
+        singlepriceText.setText(buff[2]);
+        bulkpriceText.setText(buff2[0]);
+        productID.setText(buff3);
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT description FROM product WHERE productID = "+buff3);
+        while (rs.next()){
+            productDescLabel.setText(rs.getString("description"));
+        }
+    }
+    public void selectItemsoftdrinks() throws SQLException {
+        String selectedItem = softdrinksproducts.getSelectionModel().getSelectedItem().toString();
+        String buff[] = selectedItem.split(",");
+        System.out.println(buff[0]);
+        String buff3 = buff[0].substring(1);
+        System.out.println(buff3);
+        String buff2[] = buff[3].split("]");
+        productnameText.setText(buff[1]);
+        singlepriceText.setText(buff[2]);
+        bulkpriceText.setText(buff2[0]);
+        productID.setText(buff3);
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT description FROM product WHERE productID = "+buff3);
+        while (rs.next()){
+            productDescLabel.setText(rs.getString("description"));
+        }
+    }
+    public void selectItemenergydrinks() throws SQLException {
+        String selectedItem = energydrinksproducts.getSelectionModel().getSelectedItem().toString();
+        String buff[] = selectedItem.split(",");
+        System.out.println(buff[0]);
+        String buff3 = buff[0].substring(1);
+        System.out.println(buff3);
+        String buff2[] = buff[3].split("]");
+        productnameText.setText(buff[1]);
+        singlepriceText.setText(buff[2]);
+        bulkpriceText.setText(buff2[0]);
+        productID.setText(buff3);
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT description FROM product WHERE productID = "+buff3);
+        while (rs.next()){
+            productDescLabel.setText(rs.getString("description"));
+        }
+    }
+
+    public void createCoffeeTable() throws SQLException {
+        Statement stmt = con.createStatement();
+
+        //Execute Query for the CoffeeTable data
+        ResultSet rs = stmt.executeQuery("SELECT productID,productname,singleprice,bulkprice,instock FROM product WHERE category LIKE ('Coffee') AND instock <> 0");
+
+        //Filling CoffeeTable with data
+        for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+            // We are using non property style for making dynamic table
+            final int j = i;
+            TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
+            col.setCellValueFactory(
+                    new Callback<
+                            TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                        public ObservableValue<String> call(
+                                TableColumn.CellDataFeatures<ObservableList, String> param) {
+                            return new SimpleStringProperty(param.getValue().get(j).toString());
+                        }
+                    });
+            coffeeproducts.getColumns().addAll(col);
+            System.out.println("Column [" + i + "] ");
+        }
+        while(rs.next()){
+            // Iterate Row
+            ObservableList<String> row = FXCollections.observableArrayList();
+            for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                // Iterate Column
+                row.add(rs.getString(i));
+            }
+            System.out.println("Row [1] added " + row);
+            coffeeproductsList.add(row);
+        }
+        coffeeproducts.setItems(coffeeproductsList);
+    }
+    public void createTeaTable() throws SQLException {
+        ////Execute Query for the TeaTable data
+        Statement stmt2 = con.createStatement();
+        ResultSet rs2 = stmt2.executeQuery("SELECT productID,productname,singleprice,bulkprice,instock FROM product WHERE category LIKE ('Tea') AND instock <> 0");
+
+        //Filling TeaTable with data
+        for (int i = 0; i < rs2.getMetaData().getColumnCount(); i++) {
+            // We are using non property style for making dynamic table
+            final int j = i;
+            TableColumn col = new TableColumn(rs2.getMetaData().getColumnName(i + 1));
+            col.setCellValueFactory(
+                    new Callback<
+                            TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                        public ObservableValue<String> call(
+                                TableColumn.CellDataFeatures<ObservableList, String> param) {
+                            return new SimpleStringProperty(param.getValue().get(j).toString());
+                        }
+                    });
+            teaproducts.getColumns().addAll(col);
+            System.out.println("Column [" + i + "] ");
+        }
+        while(rs2.next()){
+            // Iterate Row
+            ObservableList<String> row = FXCollections.observableArrayList();
+            for (int i = 1; i <= rs2.getMetaData().getColumnCount(); i++) {
+                // Iterate Column
+                row.add(rs2.getString(i));
+            }
+            System.out.println("Row [1] added " + row);
+            teaproductsList.add(row);
+        }
+        teaproducts.setItems(teaproductsList);
+    }
+    public void createSoftDrinksTable() throws SQLException {
+        ////Execute Query for the SoftDrinksTable data
+        Statement stmt3 = con.createStatement();
+        ResultSet rs3 = stmt3.executeQuery("SELECT productID,productname,singleprice,bulkprice,instock FROM product WHERE category LIKE ('Soft Drinks') AND instock <> 0");
+
+        //Filling SoftDrinksTable with data
+        for (int i = 0; i < rs3.getMetaData().getColumnCount(); i++) {
+            // We are using non property style for making dynamic table
+            final int j = i;
+            TableColumn col = new TableColumn(rs3.getMetaData().getColumnName(i + 1));
+            col.setCellValueFactory(
+                    new Callback<
+                            TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                        public ObservableValue<String> call(
+                                TableColumn.CellDataFeatures<ObservableList, String> param) {
+                            return new SimpleStringProperty(param.getValue().get(j).toString());
+                        }
+                    });
+            softdrinksproducts.getColumns().addAll(col);
+            System.out.println("Column [" + i + "] ");
+        }
+        while(rs3.next()){
+            // Iterate Row
+            ObservableList<String> row = FXCollections.observableArrayList();
+            for (int i = 1; i <= rs3.getMetaData().getColumnCount(); i++) {
+                // Iterate Column
+                row.add(rs3.getString(i));
+            }
+            System.out.println("Row [1] added " + row);
+            softdrinksproductsList.add(row);
+        }
+        softdrinksproducts.setItems(softdrinksproductsList);
+    }
+    public void createEnergyDrinksTable() throws SQLException {
+        //Execute Query for the EnergyDrinksTable data
+        Statement stmt4 = con.createStatement();
+        ResultSet rs4 = stmt4.executeQuery("SELECT productID,productname,singleprice,bulkprice,instock FROM product WHERE category LIKE ('Energy Drinks') AND instock <> 0");
+
+        //Filling SoftDrinksTable with data
+        for (int i = 0; i < rs4.getMetaData().getColumnCount(); i++) {
+            // We are using non property style for making dynamic table
+            final int j = i;
+            TableColumn col = new TableColumn(rs4.getMetaData().getColumnName(i + 1));
+            col.setCellValueFactory(
+                    new Callback<
+                            TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                        public ObservableValue<String> call(
+                                TableColumn.CellDataFeatures<ObservableList, String> param) {
+                            return new SimpleStringProperty(param.getValue().get(j).toString());
+                        }
+                    });
+            energydrinksproducts.getColumns().addAll(col);
+            System.out.println("Column [" + i + "] ");
+        }
+        while(rs4.next()){
+            // Iterate Row
+            ObservableList<String> row = FXCollections.observableArrayList();
+            for (int i = 1; i <= rs4.getMetaData().getColumnCount(); i++) {
+                // Iterate Column
+                row.add(rs4.getString(i));
+            }
+            System.out.println("Row [1] added " + row);
+            energydrinksproductsList.add(row);
+        }
+        energydrinksproducts.setItems(energydrinksproductsList);
+    }
+
+    public void addtoshoppingcart(){
+        float total;
+        int amount = Integer.parseInt(amountText.getText());
+        if(amount<10){
+            total = amount*Float.parseFloat(singlepriceText.getText());
+
+        }else{
+            total = amount*Float.parseFloat(bulkpriceText.getText());
+        }
+        shoppingcartList.add("No. "+productID.getText()+" productname: "+productnameText.getText()+" "+"amount: "+amountText.getText()+" total: "+total+"€");
+        shoppingcart.setItems(shoppingcartList);
+        shoppingcart.refresh();
+        sum += total;
+        totalLabel.setText("Total:"+sum+"€");
+    }
+    public void deletefromshoppingcart(){
+        shoppingcart.getItems().remove(shoppingcart.getSelectionModel().getSelectedItem());
+        sum -= buffsum;
+        totalLabel.setText("Total:"+sum+"€");
+    }
+    public void updateamount() throws SQLException {
+        sum-=buffsum;
+        shoppingcart.getItems().remove(pos);
+        float total;
+        int amount = Integer.parseInt(amountText.getText());
+        if(amount<10){
+            total = amount*Float.parseFloat(singlepriceText.getText());
+
+        }else{
+            total = amount*Float.parseFloat(bulkpriceText.getText());
+        }
+        shoppingcartList.add("No. "+productID.getText()+" productname: "+productnameText.getText()+" "+"amount: "+amountText.getText()+" total: "+total+"€");
+        shoppingcart.setItems(shoppingcartList);
+        shoppingcart.refresh();
+        sum+=total;
+        totalLabel.setText("Total:"+sum+"€");
+    }
+    public void selectitemshoppingcart() throws SQLException {
+        pos = shoppingcart.getSelectionModel().getSelectedIndex();
+        Statement stmt = con.createStatement();
+        String buff = shoppingcart.getSelectionModel().getSelectedItem().toString();
+        System.out.println(buff);
+        String buff2[] = buff.split("No. ");
+        String buff3[] = buff2[1].split(" productname: ");
+        String buff4[] = buff3[1].split(" amount: ");
+        String buff5[] = buff4[1].split(" total");
+        System.out.println(buff3[0]);
+        String buffID = buff3[0];
+        System.out.println(buff2[1]);
+        productID.setText(buffID);
+        productnameText.setText(buff4[0]);
+        amountText.setText(buff5[0]);
+        ResultSet rs = stmt.executeQuery("SELECT singleprice,bulkprice FROM product WHERE productID = "+buffID);
+        while (rs.next()){
+            singlepriceText.setText(rs.getString("singleprice"));
+            bulkpriceText.setText(rs.getString("bulkprice"));
+        }
+        int amount = Integer.parseInt(amountText.getText());
+        if(amount<10){
+            buffsum = amount*Float.parseFloat(singlepriceText.getText());
+
+        }else{
+            buffsum = amount*Float.parseFloat(bulkpriceText.getText());
+        }
+    }
+
+    public void userlogin() throws SQLException {
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT orderID,status,total,date FROM ordertab WHERE clientID = "+clientID.getText());
+        for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+            // We are using non property style for making dynamic table
+            final int j = i;
+            TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
+            col.setCellValueFactory(
+                    new Callback<
+                            TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                        public ObservableValue<String> call(
+                                TableColumn.CellDataFeatures<ObservableList, String> param) {
+                            return new SimpleStringProperty(param.getValue().get(j).toString());
+                        }
+                    });
+            orderListTable.getColumns().addAll(col);
+            System.out.println("Column [" + i + "] ");
+        }
+        while(rs.next()){
+            // Iterate Row
+            ObservableList<String> row = FXCollections.observableArrayList();
+            for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                // Iterate Column
+                row.add(rs.getString(i));
+            }
+            System.out.println("Row [1] added " + row);
+            orderList.add(row);
+        }
+        orderListTable.setItems(orderList);
+    }
+
+}
