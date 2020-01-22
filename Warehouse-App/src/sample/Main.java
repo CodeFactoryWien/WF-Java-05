80f590e00de3c76686ba56348b39cd177186cf27
Markus Gehmayer <markus.gehmayer@gmx.at>
2020-01-22 18:15:27 +0100

    upd client-app tableview and user orders-warehouse app fxml

--- Warehouse-App/src/sample/Main.java
+++ Warehouse-App/src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Warehouse-App");
-        primaryStage.setScene(new Scene(root, 1600, 800));
+        primaryStage.setTitle("Warehouse-App Feel Good Ltd.");
+        primaryStage.setScene(new Scene(root, 1200, 700));
         primaryStage.show();
     }
 
